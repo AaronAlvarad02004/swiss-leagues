dda4f0433b654a6963cae9a2e21595a32eecd56f
salvaa <salvaa@bbcag.ch>
2025-02-18 14:55:31 +0100

    bottomnagivation

---
+++ swiss_leagues/app/tabs/_layout.js
@@ -0,0 +1,65 @@
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons"
+
+export default function TabsLayout(){
+    return(
+        <Tabs>
+            <Tabs.Screen
+                name="matches/index"
+                options={{
+                    title:"Matches",
+                    tabBarIcon: ({Color}) => (
+                        <Ionicons
+                            size={28}
+                            style={{marginBottom: -3}}
+                            name="football-outline"
+                            color={Color}
+                        />
+                    )
+                }}
+            />
+             <Tabs.Screen
+                name="leagues/index"
+                options={{
+                    title:"Leagues",
+                    tabBarIcon: ({Color}) => (
+                        <Ionicons
+                            size={28}
+                            style={{marginBottom: -3}}
+                            name="trophy-outline"
+                            color={Color}
+                        />
+                    )
+                }}
+            />
+                <Tabs.Screen
+                name="favorites/index"
+                options={{
+                    title:"Favorites",
+                    tabBarIcon: ({Color}) => (
+                        <Ionicons
+                            size={28}
+                            style={{marginBottom: -3}}
+                            name="star-outline"
+                            color={Color}
+                        />
+                    )
+                }}
+            />
+                <Tabs.Screen
+                name="settings/index"
+                options={{
+                    title:"Settings",
+                    tabBarIcon: ({Color}) => (
+                        <Ionicons
+                            size={28}
+                            style={{marginBottom: -3}}
+                            name="cog-outline"
+                            color={Color}
+                        />
+                    )
+                }}
+            />
+        </Tabs>
+    )
+}
