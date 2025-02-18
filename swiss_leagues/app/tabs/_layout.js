f8e4644d8e7e543e8d648e33b0bf63a2c1929b7f
Eray da Silva Burmaci <sdasie@bbcag.ch>
2025-02-18 16:33:19 +0100

    Created CustomColors.js

--- swiss_leagues/app/tabs/_layout.js
+++ swiss_leagues/app/tabs/_layout.js
@@ -1,5 +1,6 @@
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons"
+import CustomColors from "/../components/CustomColors.js";
 
 export default function TabsLayout(){
     return(
